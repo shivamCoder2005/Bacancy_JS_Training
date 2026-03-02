78329a58d641444e072bccfff486cca2d75b9178
makwanashivam2005 <makwanashivam1709@gmail.com>
2026-03-02 13:10:24 +0530

    session_12 assignment 5 solved

--- session_12/assignment5.ts
+++ session_12/assignment5.ts
@@ -1,13 +1,16 @@
 // Create a constructor type for UserRepository .
 
+type User = {
+    id: number,
+    name: string
+    email: string
+}
+
 abstract class BaseRepository<T> {
     abstract getById(id: string): T
 }
 
 class UserRepository extends BaseRepository<User> {
-    constructor(public repoName: string, public repoSize: number) {
-        super()
-    }
     getById(id: string): User {
         return {
             id: 5,
@@ -21,10 +24,11 @@ type ctrType<T> = new (...args: any[]) => T
 
 type UserRepositoryCtr = ctrType<UserRepository>
 
-
-function createInstance(ctrFn: UserRepositoryCtr): UserRepository {
-    return new ctrFn()
+function createUserInstance(ctr: UserRepositoryCtr): UserRepository {
+    return new ctr()
 }
+
+createUserInstance(UserRepository)
 
 // more general verison
 
@@ -32,7 +36,6 @@ function createInstance(ctrFn: UserRepositoryCtr): UserRepository {
 //     return new ctrFn(...args)
 // }
 
-createInstance(UserRepository)
 
 // Create a callable type that formats User name.
 
@@ -46,6 +49,8 @@ const nameFormatter: Formatter = (name: string) => {
 }
 
 nameFormatter.greetMsg = "hii"
+
+
 
 
 
